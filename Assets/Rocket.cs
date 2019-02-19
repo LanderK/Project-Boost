a468ee0e7168822a41dfa8761e8d3c97caae6b42
LanderK <landerking@gmail.com>
2019-02-19 20:35:34 +0000

    Thrust Added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        //can trust while rotatiing
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            print("Right");
+        }
+    }
+}
