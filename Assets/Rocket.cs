915b17ecd017795fd7a28a7c5dcc8dfe55db0f3a
LanderK <landerking@gmail.com>
2019-02-19 21:08:35 +0000

    Rotation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -26,10 +26,10 @@ public class Rocket : MonoBehaviour {
         }
 
         if (Input.GetKey(KeyCode.A)) {
-            print("Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)) {
-            print("Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
