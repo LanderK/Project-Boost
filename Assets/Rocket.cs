a8868b0c3c7b995bfb5bd75e5855b809a5212399
LanderK <landerking@gmail.com>
2019-02-21 21:10:44 +0000

    Fields and Tags

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 25f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
-
-	// Use this for initialization
-	void Start () {
+    
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
@@ -20,11 +24,27 @@ public class Rocket : MonoBehaviour {
         Rotate();
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("Okay");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("DEAD");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            //float thrustThisFrame = mainThrust * Time.deltaTime;
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -42,17 +62,19 @@ public class Rocket : MonoBehaviour {
 
         rigidBody.freezeRotation = true; // take manual control of rotation.
 
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // Resume physics control of rotation
     }
 
-    
 }
